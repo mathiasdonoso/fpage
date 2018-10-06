a92e9c07b473ef5e855ef8893eebf8d93d2343d8
Mathías <mathiasd88@gmail.com>
2018-10-06 02:53:45 -0300

    Add: new dependencies and pug as template engine

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express')
 const app = express()
 const port = 3000 || process.env.PORT
 
+app.set('view engine', 'pug')
+
 require('./src/common/middlewares')(app)
 
 app.use(require('./src/features'))
